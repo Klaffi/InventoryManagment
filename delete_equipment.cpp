be6e0e637809161060f0d82cb88e8435557e7dab
nfoth <36969331+Klaffi@users.noreply.github.com>
2026-02-23 09:20:31 +0300

    Update delete_equipment.cpp

--- delete_equipment.cpp
+++ delete_equipment.cpp
@@ -9,7 +9,7 @@ delete_equipment::delete_equipment(QWidget *parent)
     db = QSqlDatabase::addDatabase("QPSQL");
     db.setDatabaseName("accs");
     db.setUserName("postgres");
-    db.setPassword("Urysapape1");
+    db.setPassword("xxxx");
     if (!db.open()) {
         qDebug() << "Error: connection with database fail";
     } else {
