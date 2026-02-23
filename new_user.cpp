101b402df88046fc29c6596a46367d7065d05b33
nfoth <36969331+Klaffi@users.noreply.github.com>
2026-02-23 09:21:51 +0300

    Update new_user.cpp

--- new_user.cpp
+++ new_user.cpp
@@ -10,7 +10,7 @@ new_user::new_user(QWidget *parent)
     db = QSqlDatabase::addDatabase("QPSQL");
     db.setDatabaseName("accs");
     db.setUserName("postgres");
-    db.setPassword("Urysapape1");
+    db.setPassword("xxxx");
 
     if (!db.open()) {
         qDebug() << "Error: connection with database fail";
