79eca9c5471ff21ce38e035892e6284759f65766
nfoth <36969331+Klaffi@users.noreply.github.com>
2026-02-23 09:20:07 +0300

    Update edit_story.cpp

--- edit_story.cpp
+++ edit_story.cpp
@@ -15,7 +15,7 @@ edit_story::~edit_story()
     db = QSqlDatabase::addDatabase("QPSQL");
     db.setDatabaseName("accs");
     db.setUserName("postgres");
-    db.setPassword("Urysapape1");
+    db.setPassword("xxxx");
     if (!db.open()) {
         qDebug() << "Error: connection with database fail";
     } else {
